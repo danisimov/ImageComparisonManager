a4e63498dddd1ad16954d2628141e6c456d7fc78
danisimov <d.anisimovq+g@gmail.com>
2017-08-06 19:37:36 +0300

    add logic exception

--- src/main/java/service/ImageComparisonService.java
+++ src/main/java/service/ImageComparisonService.java
@@ -1,10 +1,12 @@
 package service;
 
 import org.im4java.core.CompareCmd;
+import org.im4java.core.IM4JavaException;
 import org.im4java.core.IMOperation;
 import org.im4java.process.StandardStream;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by danisimov on 4/4/16.
@@ -35,9 +37,13 @@ class ImageComparisonService {
         try {
             compare.run(cmpOp);
             return true;
-        }
-        catch (Exception ex) {
+        } catch (InterruptedException e) {
+            e.printStackTrace();
             System.out.println("Check images/$testSuiteName/difference.png file, to see the difference report and read the message above for details");
+            return false;
+        } catch (IOException | IM4JavaException e) {
+            e.printStackTrace();
+            System.out.println("Please be sure that ImageMagick is installed, you pass images to manager, and read the message above for details");
             return false;
         }
     }
